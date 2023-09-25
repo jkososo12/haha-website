e2b92dd177976ec9b03faff2d10f4575fd62369b
jkososo12 <145211137+jkososo12@users.noreply.github.com>
2023-09-25 20:21:34 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const ucapan = document.getElementById('btn');
+ucapan.addEventListener('click', function(){
+    alert('Sama-sama! ~Aleesya')
+})
+const para = document.querySelector('h1')
+para.addEventListener('mouseenter', function(){
+    para.innerHTML = "Iya Ayah Ulang tahun :b"
+})
+para.addEventListener('mouseleave', function(){
+    para.innerHTML = 'Happy Birthday Dad-!!'
+})
